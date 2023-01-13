c7ad5a815f99dcb6bc9ba1cb5b8cd8fedd188651
SamuelNdreu <samwellsoficcial@gmail.com>
2023-01-13 16:54:11 +0100

    upgrading project

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,24 @@
-<script setup>
-
+<script>
+import AppMain from './components/AppMain.vue'
+// import AppLoader from './components/AppLoader.vue'
+export default {
+    name: "App",
+    components: {
+        AppMain,
+        // AppLoader,
+    }
+}
 </script>
 
 <template>
-  
-
+    <main>
+        <!-- <AppLoader /> -->
+        <AppMain />
+    </main>
+    
 </template>
 
 <style scoped>
+
 
 </style>
